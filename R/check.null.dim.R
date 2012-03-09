a666216b25e9378faf8336531f36aeeee4f3b52e
Juan C. Vivar <jvivar@nccu.edu>
2012-03-09 00:00:00 +0000

    version 2.1

--- R/check.null.dim.R
+++ R/check.null.dim.R
@@ -1,9 +1,15 @@
 check.null.dim <-
 function(vec0){
-	if(is.null(dim(vec0))){
-		lov <- length(vec0)
-		vec0 <- matrix(vec0,1,lov)
-	}
+
+if(is.null(dim(vec0))){
+
+lov <- length(vec0)
+
+vec0 <- matrix(vec0,1,lov)
+
+}
+
 return(vec0)
+
 }
 
